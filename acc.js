048b7b0a9551f1ffaf54eaa9ea2688a2934063e9
Rpanguana <filipe.panguana@outlook.com>
2019-10-08 14:51:53 +0200

    acc.js

---
+++ acc.js
@@ -0,0 +1,42 @@
+class BankAccount {
+    constructor(balance, interest, monthlyFee){
+        this.balance = balance;
+        this.interest = interest;
+        this.monthlyFee = monthlyFee;
+    }
+
+
+finishMonth(){
+// var grossAmount = (this.balance + this.deposit()) - withdraw();
+// var totalAmount = grossAmount * this.interest;
+let rate = 1 + (this.interest/100);
+return this.balance = parseInt(rate * this.balance - this.monthlyFee);
+
+// return totalAmount;
+}
+
+deposit(amount){
+this.balance = this.balance + amount;
+
+return this.balance;
+}
+
+withdraw(amount){
+this.balance = this.balance - amount;
+
+return this.balance;
+}
+}
+
+let bankAccount = new BankAccount(100,12, 50 ); //balance, interest, monthlyFee
+// bankAccount.deposit();
+// bankAccount.withdraw();
+
+console.log(bankAccount.finishMonth());
+console.log(bankAccount.deposit(100));
+console.log(bankAccount.withdraw(50));
+
+
+class {
+    
+}
