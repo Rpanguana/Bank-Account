0dae1690723593c27a9c33a56e364642d1b7a47d
Rpanguana <filipe.panguana@outlook.com>
2019-10-24 16:37:05 +0200

    some progress, transfer in bank.js needs improvements

--- jasmine-standalone-3.5.0/src/bankacc.js
+++ jasmine-standalone-3.5.0/src/bankacc.js
@@ -23,40 +23,15 @@ return this.balance;
 
 withdraw(amount){
 this.balance = this.balance - amount;
-
 return this.balance;
 }
-}
-
-let bankAccount = new BankAccount(100,12, 50 ); //balance, interest, monthlyFee
-// bankAccount.deposit();
-// bankAccount.withdraw();
-
-console.log(bankAccount.finishMonth());
-console.log(bankAccount.deposit(100));
-console.log(bankAccount.withdraw(50));
-
-/////////
-
-class Bank extends BankAccount {
-    constructor(balance, interest, monthlyFee){
-        let bankacc = new BankAccount(balance, interest, monthlyFee);
-    }
-
-withdraw(bankAccountNumber,amount){ 
-// require an acccount number, validate it and require withdrawal amount.
 
 }
 
-deposit(bankAccountNumber,amount){
-// require account number, validate it and require deposit amount.
+let bankAccount = new BankAccount(100,12, 50); //balance, interest, monthlyFee
 
-}
+//console.log(bankAccount.finishMonth());
+//console.log(bankAccount.deposit(100));
+//console.log(bankAccount.withdraw(50));
 
-transfer(from_bank_account_number,to_bank_account_number, amount){
-// require FROM bank account number, validate it and require TO bank account number and validate it.
-// require amount and act withdraw on FROM account.
-// act deposit on TO account.... 
-    
-}
-}
+module.exports = BankAccount;
