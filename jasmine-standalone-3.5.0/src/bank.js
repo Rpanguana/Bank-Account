ea46c1cce5f5d9408b82731d91e0ceea601aaf02
Rpanguana <filipe.panguana@outlook.com>
2019-10-28 17:10:17 +0200

    trying to solve a bug

--- jasmine-standalone-3.5.0/src/bank.js
+++ jasmine-standalone-3.5.0/src/bank.js
@@ -11,8 +11,9 @@ class Bank {
 createAccounts(balance, interest, monthlyFee, accountNumber){
     this.bankAccount = new BankAccount(balance, interest, monthlyFee);
     this.accountNumber = accountNumber;
-    accounts.push({accountnumber : this.accountNumber,
-    bankAccount : this.bankAccount
+    accounts.push({
+        accountnumber : this.accountNumber,
+        bankAccount : this.bankAccount
     })
 }
 
@@ -35,39 +36,36 @@ for (let j = 0; j < accounts.length; j++){
 }}
 
 transfer(from_bank_account_number, to_bank_account_number, amount){
-// require FROM bank account number, validate it and require TO bank account number and validate it.
-// require amount and act withdraw on FROM account.
-// act deposit on TO account.... 
-// bankAccountNumber = from_bank_account_number;
+// var deposit = this.balance = this.balance - amount
 for (let k =0; k < accounts.length; k++){
     if (from_bank_account_number === this.accountNumber ){
         accounts[k].bankAccount.withdraw(amount);
-        
         for (let l =0; l < accounts.length; l++){
             if (to_bank_account_number === this.accountNumber){
                 accounts[l].bankAccount.deposit(amount);
-            } return this.bankAccount.balance;
-        }}
-    } return this.bankAccount.balance;
+                return this.bankAccount.balance;
+            }
+}
 }
 
 }
 
+
+}}
 
 /*.............................................instances of bank. BANK ACCOUNTS.......................................*/
 
-let client = new Bank(20000, 10, 50, 1234567899);//balance, interest, monthlyFee, accountNumber
-let client2 = new Bank(3000, 10, 50, 9987654321)
-let client3 = new Bank(11357, 10, 50, 7532654321)
+let client = new Bank(200, 10, 50, 1234567899);//balance, interest, monthlyFee, accountNumber
+let client2 = new Bank(100, 10, 50, 9987654321)
+// let client3 = new Bank(50, 10, 50, 7532654321)
 
 console.log(accounts)
+//client.withdraw(1234567899, 100)
+console.log("---------------------------------");
 
 //console.log("see below")
-
 // console.log(client.transfer(client.accountNumber, client2.accountNumber, 100));
-
-console.log(client.transfer(client.accountNumber,client2.accountNumber, 100))
-
+console.log(client.transfer(client.accountNumber, client2.accountNumber, 100))
 //console.log("see below")
-
+// console.log(accounts)
 console.log(accounts)
--- jasmine-standalone-3.5.0/src/bankacc.js
+++ jasmine-standalone-3.5.0/src/bankacc.js
@@ -5,19 +5,16 @@ class BankAccount {
         this.monthlyFee = monthlyFee;
     }
 
-
 finishMonth(){
 // var grossAmount = (this.balance + this.deposit()) - withdraw();
 // var totalAmount = grossAmount * this.interest;
 let rate = 1 + (this.interest/100);
 return this.balance = parseInt(rate * this.balance - this.monthlyFee);
-
 // return totalAmount;
 }
 
 deposit(amount){
 this.balance = this.balance + amount;
-
 return this.balance;
 }
 
@@ -25,11 +22,9 @@ withdraw(amount){
 this.balance = this.balance - amount;
 return this.balance;
 }
-
 }
 
 let bankAccount = new BankAccount(100,12, 50); //balance, interest, monthlyFee
-
 //console.log(bankAccount.finishMonth());
 //console.log(bankAccount.deposit(100));
 //console.log(bankAccount.withdraw(50));
